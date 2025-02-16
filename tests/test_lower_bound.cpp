f1cf5892be02dd3b3ab160f11a7176990cbe6d08
黃泓量 Jeremy <jmw@netvigator.com>
2025-02-16 21:44:45 +0800

    Use Google Test

--- tests/test_lower_bound.cpp
+++ tests/test_lower_bound.cpp
@@ -1,28 +1,28 @@
-#include <iostream>
+#include <gtest/gtest.h>
 #include <vector>
-#include <cassert>
 #include "lower_bound.hpp"
 
-void test_lower_bound() {
-    // Test with integers
+TEST(LowerBoundTest, Integers) {
     std::vector<int> vec = {1, 2, 4, 5, 6};
     std::vector<int> test_values = {3, 0, 7};
     std::vector<size_t> expected_indices = {2, 0, 5};
     for (size_t i = 0; i < test_values.size(); ++i) {
         auto it = jrmwng::algorithm::lower_bound(vec.begin(), vec.end(), test_values[i], std::less<int>());
-        assert(it == vec.begin() + expected_indices[i]);
+        EXPECT_EQ(it, vec.begin() + expected_indices[i]);
     }
+}
 
-    // Test with doubles
+TEST(LowerBoundTest, Doubles) {
     std::vector<double> vec_d = {1.1, 2.2, 4.4, 5.5, 6.6};
     std::vector<double> test_values_d = {3.3, 0.0, 7.7};
     std::vector<size_t> expected_indices_d = {2, 0, 5};
     for (size_t i = 0; i < test_values_d.size(); ++i) {
         auto it_d = jrmwng::algorithm::lower_bound(vec_d.begin(), vec_d.end(), test_values_d[i], std::less<double>());
-        assert(it_d == vec_d.begin() + expected_indices_d[i]);
+        EXPECT_EQ(it_d, vec_d.begin() + expected_indices_d[i]);
     }
+}
 
-    // Test with custom predicate
+TEST(LowerBoundTest, CustomPredicate) {
     struct CustomType {
         int value;
         bool operator<(const CustomType& other) const {
@@ -35,134 +35,100 @@ void test_lower_bound() {
     std::vector<size_t> expected_indices_custom = {2, 0, 3};
     for (size_t i = 0; i < test_values_custom.size(); ++i) {
         auto it_custom = jrmwng::algorithm::lower_bound(custom_vec.begin(), custom_vec.end(), test_values_custom[i], std::less<CustomType>());
-        assert(it_custom == custom_vec.begin() + expected_indices_custom[i]);
+        EXPECT_EQ(it_custom, custom_vec.begin() + expected_indices_custom[i]);
     }
+}
 
-    // Test with empty vector
+TEST(LowerBoundTest, EmptyVector) {
     std::vector<int> empty_vec;
     auto it_empty = jrmwng::algorithm::lower_bound(empty_vec.begin(), empty_vec.end(), 1);
-    assert(it_empty == empty_vec.begin()); // 1 should be inserted at index 0
+    EXPECT_EQ(it_empty, empty_vec.begin());
+}
 
-    // Test with single element vector
+TEST(LowerBoundTest, SingleElementVector) {
     std::vector<int> single_vec = {2};
     std::vector<int> test_values_single = {1, 3};
     std::vector<size_t> expected_indices_single = {0, 1};
     for (size_t i = 0; i < test_values_single.size(); ++i) {
         auto it_single = jrmwng::algorithm::lower_bound(single_vec.begin(), single_vec.end(), test_values_single[i]);
-        assert(it_single == single_vec.begin() + expected_indices_single[i]);
+        EXPECT_EQ(it_single, single_vec.begin() + expected_indices_single[i]);
     }
+}
 
-    // Test with all elements equal
+TEST(LowerBoundTest, AllElementsEqual) {
     std::vector<int> equal_vec = {2, 2, 2, 2};
     std::vector<int> test_values_equal = {2, 1, 3};
     std::vector<size_t> expected_indices_equal = {0, 0, 4};
     for (size_t i = 0; i < test_values_equal.size(); ++i) {
         auto it_equal = jrmwng::algorithm::lower_bound(equal_vec.begin(), equal_vec.end(), test_values_equal[i]);
-        assert(it_equal == equal_vec.begin() + expected_indices_equal[i]);
+        EXPECT_EQ(it_equal, equal_vec.begin() + expected_indices_equal[i]);
     }
-
-    // Test range-based lower_bound with integers
-    for (size_t i = 0; i < test_values.size(); ++i) {
-        auto it_range = jrmwng::algorithm::ranges::lower_bound(vec, test_values[i], std::less<int>(), [](int i) { return i; });
-        assert(it_range == vec.begin() + expected_indices[i]);
-    }
-
-    // Test range-based lower_bound with doubles
-    for (size_t i = 0; i < test_values_d.size(); ++i) {
-        auto it_d_range = jrmwng::algorithm::ranges::lower_bound(vec_d, test_values_d[i], std::less<double>(), [](double d) { return d; });
-        assert(it_d_range == vec_d.begin() + expected_indices_d[i]);
-    }
-
-    // Test range-based lower_bound with custom predicate
-    for (size_t i = 0; i < test_values_custom.size(); ++i) {
-        auto it_custom_range = jrmwng::algorithm::ranges::lower_bound(custom_vec, test_values_custom[i], std::less<CustomType>(), [](const CustomType& ct) { return ct; });
-        assert(it_custom_range == custom_vec.begin() + expected_indices_custom[i]);
-    }
-
-    // Test range-based lower_bound with empty vector
-    auto it_empty_range = jrmwng::algorithm::ranges::lower_bound(empty_vec, 1, std::less<int>(), [](int i) { return i; });
-    assert(it_empty_range == empty_vec.begin()); // 1 should be inserted at index 0
-
-    // Test range-based lower_bound with single element vector
-    for (size_t i = 0; i < test_values_single.size(); ++i) {
-        auto it_single_range = jrmwng::algorithm::ranges::lower_bound(single_vec, test_values_single[i], std::less<int>(), [](int i) { return i; });
-        assert(it_single_range == single_vec.begin() + expected_indices_single[i]);
-    }
-
-    // Test range-based lower_bound with all elements equal
-    for (size_t i = 0; i < test_values_equal.size(); ++i) {
-        auto it_equal_range = jrmwng::algorithm::ranges::lower_bound(equal_vec, test_values_equal[i], std::less<int>(), [](int i) { return i; });
-        assert(it_equal_range == equal_vec.begin() + expected_indices_equal[i]);
-    }
-
-    // Test default arguments with integers
-    for (size_t i = 0; i < test_values.size(); ++i) {
-        auto it_default = jrmwng::algorithm::lower_bound(vec.begin(), vec.end(), test_values[i]);
-        assert(it_default == vec.begin() + expected_indices[i]);
-    }
-
-    // Test default arguments with doubles
-    for (size_t i = 0; i < test_values_d.size(); ++i) {
-        auto it_d_default = jrmwng::algorithm::lower_bound(vec_d.begin(), vec_d.end(), test_values_d[i]);
-        assert(it_d_default == vec_d.begin() + expected_indices_d[i]);
-    }
-
-    // Test default arguments with custom predicate
-    for (size_t i = 0; i < test_values_custom.size(); ++i) {
-        auto it_custom_default = jrmwng::algorithm::lower_bound(custom_vec.begin(), custom_vec.end(), test_values_custom[i]);
-        assert(it_custom_default == custom_vec.begin() + expected_indices_custom[i]);
-    }
-
-    // Test default arguments with empty vector
-    auto it_empty_default = jrmwng::algorithm::lower_bound(empty_vec.begin(), empty_vec.end(), 1);
-    assert(it_empty_default == empty_vec.begin()); // 1 should be inserted at index 0
-
-    // Test default arguments with single element vector
-    for (size_t i = 0; i < test_values_single.size(); ++i) {
-        auto it_single_default = jrmwng::algorithm::lower_bound(single_vec.begin(), single_vec.end(), test_values_single[i]);
-        assert(it_single_default == single_vec.begin() + expected_indices_single[i]);
-    }
-
-    // Test default arguments with all elements equal
-    for (size_t i = 0; i < test_values_equal.size(); ++i) {
-        auto it_equal_default = jrmwng::algorithm::lower_bound(equal_vec.begin(), equal_vec.end(), test_values_equal[i]);
-        assert(it_equal_default == equal_vec.begin() + expected_indices_equal[i]);
-    }
-
-    // Test range-based lower_bound with default arguments
-    for (size_t i = 0; i < test_values.size(); ++i) {
-        auto it_range_default = jrmwng::algorithm::ranges::lower_bound(vec, test_values[i]);
-        assert(it_range_default == vec.begin() + expected_indices[i]);
-    }
-
-    for (size_t i = 0; i < test_values_d.size(); ++i) {
-        auto it_d_range_default = jrmwng::algorithm::ranges::lower_bound(vec_d, test_values_d[i]);
-        assert(it_d_range_default == vec_d.begin() + expected_indices_d[i]);
-    }
-
-    for (size_t i = 0; i < test_values_custom.size(); ++i) {
-        auto it_custom_range_default = jrmwng::algorithm::ranges::lower_bound(custom_vec, test_values_custom[i]);
-        assert(it_custom_range_default == custom_vec.begin() + expected_indices_custom[i]);
-    }
-
-    auto it_empty_range_default = jrmwng::algorithm::ranges::lower_bound(empty_vec, 1);
-    assert(it_empty_range_default == empty_vec.begin()); // 1 should be inserted at index 0
-
-    for (size_t i = 0; i < test_values_single.size(); ++i) {
-        auto it_single_range_default = jrmwng::algorithm::ranges::lower_bound(single_vec, test_values_single[i]);
-        assert(it_single_range_default == single_vec.begin() + expected_indices_single[i]);
-    }
-
-    for (size_t i = 0; i < test_values_equal.size(); ++i) {
-        auto it_equal_range_default = jrmwng::algorithm::ranges::lower_bound(equal_vec, test_values_equal[i]);
-        assert(it_equal_range_default == equal_vec.begin() + expected_indices_equal[i]);
-    }
-
-    std::cout << "All tests passed!" << std::endl;
 }
 
-int main() {
-    std::cout << "Compilation date and time: " << __DATE__ << " " << __TIME__ << std::endl;
-    test_lower_bound();
-    return 0;
+TEST(LowerBoundTest, RangeBasedIntegers) {
+    std::vector<int> vec = {1, 2, 4, 5, 6};
+    std::vector<int> test_values = {3, 0, 7};
+    std::vector<size_t> expected_indices = {2, 0, 5};
+    for (size_t i = 0; i < test_values.size(); ++i) {
+        auto it_range = jrmwng::algorithm::ranges::lower_bound(vec, test_values[i], std::less<int>(), [](int i) { return i; });
+        EXPECT_EQ(it_range, vec.begin() + expected_indices[i]);
+    }
+}
+
+TEST(LowerBoundTest, RangeBasedDoubles) {
+    std::vector<double> vec_d = {1.1, 2.2, 4.4, 5.5, 6.6};
+    std::vector<double> test_values_d = {3.3, 0.0, 7.7};
+    std::vector<size_t> expected_indices_d = {2, 0, 5};
+    for (size_t i = 0; i < test_values_d.size(); ++i) {
+        auto it_d_range = jrmwng::algorithm::ranges::lower_bound(vec_d, test_values_d[i], std::less<double>(), [](double d) { return d; });
+        EXPECT_EQ(it_d_range, vec_d.begin() + expected_indices_d[i]);
+    }
+}
+
+TEST(LowerBoundTest, RangeBasedCustomPredicate) {
+    struct CustomType {
+        int value;
+        bool operator<(const CustomType& other) const {
+            return value < other.value;
+        }
+    };
+
+    std::vector<CustomType> custom_vec = {{1}, {3}, {5}};
+    std::vector<CustomType> test_values_custom = {{4}, {0}, {6}};
+    std::vector<size_t> expected_indices_custom = {2, 0, 3};
+    for (size_t i = 0; i < test_values_custom.size(); ++i) {
+        auto it_custom_range = jrmwng::algorithm::ranges::lower_bound(custom_vec, test_values_custom[i], std::less<CustomType>(), [](const CustomType& ct) { return ct; });
+        EXPECT_EQ(it_custom_range, custom_vec.begin() + expected_indices_custom[i]);
+    }
+}
+
+TEST(LowerBoundTest, RangeBasedEmptyVector) {
+    std::vector<int> empty_vec;
+    auto it_empty_range = jrmwng::algorithm::ranges::lower_bound(empty_vec, 1, std::less<int>(), [](int i) { return i; });
+    EXPECT_EQ(it_empty_range, empty_vec.begin());
+}
+
+TEST(LowerBoundTest, RangeBasedSingleElementVector) {
+    std::vector<int> single_vec = {2};
+    std::vector<int> test_values_single = {1, 3};
+    std::vector<size_t> expected_indices_single = {0, 1};
+    for (size_t i = 0; i < test_values_single.size(); ++i) {
+        auto it_single_range = jrmwng::algorithm::ranges::lower_bound(single_vec, test_values_single[i], std::less<int>(), [](int i) { return i; });
+        EXPECT_EQ(it_single_range, single_vec.begin() + expected_indices_single[i]);
+    }
+}
+
+TEST(LowerBoundTest, RangeBasedAllElementsEqual) {
+    std::vector<int> equal_vec = {2, 2, 2, 2};
+    std::vector<int> test_values_equal = {2, 1, 3};
+    std::vector<size_t> expected_indices_equal = {0, 0, 4};
+    for (size_t i = 0; i < test_values_equal.size(); ++i) {
+        auto it_equal_range = jrmwng::algorithm::ranges::lower_bound(equal_vec, test_values_equal[i], std::less<int>(), [](int i) { return i; });
+        EXPECT_EQ(it_equal_range, equal_vec.begin() + expected_indices_equal[i]);
+    }
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
--- tests/test_lower_bound_simd.cpp
+++ tests/test_lower_bound_simd.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
+#include <gtest/gtest.h>
 #include <vector>
-#include <cassert>
 #include "lower_bound_simd.hpp"
 
-void test_lower_bound_simd() {
-    // Test SIMD lower_bound with integers
+TEST(LowerBoundSimdTest, Integers) {
     std::vector<int> vec = {1, 2, 4, 5, 6};
     auto it_simd = jrmwng::algorithm::simd::lower_bound(vec, 3);
-    assert(it_simd == vec.begin() + 2); // 3 should be inserted at index 2
+    EXPECT_EQ(it_simd, vec.begin() + 2);
+}
 
-    // Test SIMD lower_bound with doubles
+TEST(LowerBoundSimdTest, Doubles) {
     std::vector<double> vec_d = {1.1, 2.2, 4.4, 5.5, 6.6};
     auto it_d_simd = jrmwng::algorithm::simd::lower_bound(vec_d, 3.3);
-    assert(it_d_simd == vec_d.begin() + 2); // 3.3 should be inserted at index 2
+    EXPECT_EQ(it_d_simd, vec_d.begin() + 2);
+}
 
-    // Test SIMD lower_bound with custom predicate
+TEST(LowerBoundSimdTest, CustomPredicate) {
     struct CustomType {
         int value;
         bool operator<(const CustomType& other) const {
@@ -25,66 +25,86 @@ void test_lower_bound_simd() {
     std::vector<CustomType> custom_vec = {{1}, {3}, {5}};
     CustomType new_value = {4};
     auto it_custom_simd = jrmwng::algorithm::simd::lower_bound(custom_vec, new_value, std::less<CustomType>());
-    assert(it_custom_simd == custom_vec.begin() + 2); // {4} should be inserted at index 2
+    EXPECT_EQ(it_custom_simd, custom_vec.begin() + 2);
+}
 
-    // Test SIMD lower_bound with empty vector
+TEST(LowerBoundSimdTest, EmptyVector) {
     std::vector<int> empty_vec;
     auto it_empty_simd = jrmwng::algorithm::simd::lower_bound(empty_vec, 1);
-    assert(it_empty_simd == empty_vec.begin()); // 1 should be inserted at index 0
+    EXPECT_EQ(it_empty_simd, empty_vec.begin());
+}
 
-    // Test SIMD lower_bound with single element vector
+TEST(LowerBoundSimdTest, SingleElementVector) {
     std::vector<int> single_vec = {2};
     auto it_single_simd = jrmwng::algorithm::simd::lower_bound(single_vec, 1);
-    assert(it_single_simd == single_vec.begin()); // 1 should be inserted at index 0
+    EXPECT_EQ(it_single_simd, single_vec.begin());
 
     it_single_simd = jrmwng::algorithm::simd::lower_bound(single_vec, 3);
-    assert(it_single_simd == single_vec.end()); // 3 should be inserted at index 1
+    EXPECT_EQ(it_single_simd, single_vec.end());
+}
 
-    // Test SIMD lower_bound with all elements equal
+TEST(LowerBoundSimdTest, AllElementsEqual) {
     std::vector<int> equal_vec = {2, 2, 2, 2};
     auto it_equal_simd = jrmwng::algorithm::simd::lower_bound(equal_vec, 2);
-    assert(it_equal_simd == equal_vec.begin()); // 2 should be inserted at index 0
+    EXPECT_EQ(it_equal_simd, equal_vec.begin());
 
     it_equal_simd = jrmwng::algorithm::simd::lower_bound(equal_vec, 1);
-    assert(it_equal_simd == equal_vec.begin()); // 1 should be inserted at index 0
+    EXPECT_EQ(it_equal_simd, equal_vec.begin());
 
     it_equal_simd = jrmwng::algorithm::simd::lower_bound(equal_vec, 3);
-    assert(it_equal_simd == equal_vec.end()); // 3 should be inserted at index 4
+    EXPECT_EQ(it_equal_simd, equal_vec.end());
+}
 
-    // Test SIMD lower_bound with default arguments
+TEST(LowerBoundSimdTest, DefaultArguments) {
+    std::vector<int> vec = {1, 2, 4, 5, 6};
     auto it_default_simd = jrmwng::algorithm::simd::lower_bound(vec, 3);
-    assert(it_default_simd == vec.begin() + 2); // 3 should be inserted at index 2
+    EXPECT_EQ(it_default_simd, vec.begin() + 2);
 
+    std::vector<double> vec_d = {1.1, 2.2, 4.4, 5.5, 6.6};
     auto it_d_default_simd = jrmwng::algorithm::simd::lower_bound(vec_d, 3.3);
-    assert(it_d_default_simd == vec_d.begin() + 2); // 3.3 should be inserted at index 2
+    EXPECT_EQ(it_d_default_simd, vec_d.begin() + 2);
 
+    struct CustomType {
+        int value;
+        bool operator<(const CustomType& other) const {
+            return value < other.value;
+        }
+    };
+
+    std::vector<CustomType> custom_vec = {{1}, {3}, {5}};
+    CustomType new_value = {4};
     auto it_custom_default_simd = jrmwng::algorithm::simd::lower_bound(custom_vec, new_value);
-    assert(it_custom_default_simd == custom_vec.begin() + 2); // {4} should be inserted at index 2
+    EXPECT_EQ(it_custom_default_simd, custom_vec.begin() + 2);
 
+    std::vector<int> empty_vec;
     auto it_empty_default_simd = jrmwng::algorithm::simd::lower_bound(empty_vec, 1);
-    assert(it_empty_default_simd == empty_vec.begin()); // 1 should be inserted at index 0
+    EXPECT_EQ(it_empty_default_simd, empty_vec.begin());
 
+    std::vector<int> single_vec = {2};
     auto it_single_default_simd = jrmwng::algorithm::simd::lower_bound(single_vec, 1);
-    assert(it_single_default_simd == single_vec.begin()); // 1 should be inserted at index 0
+    EXPECT_EQ(it_single_default_simd, single_vec.begin());
 
     it_single_default_simd = jrmwng::algorithm::simd::lower_bound(single_vec, 3);
-    assert(it_single_default_simd == single_vec.end()); // 3 should be inserted at index 1
+    EXPECT_EQ(it_single_default_simd, single_vec.end());
 
+    std::vector<int> equal_vec = {2, 2, 2, 2};
     auto it_equal_default_simd = jrmwng::algorithm::simd::lower_bound(equal_vec, 2);
-    assert(it_equal_default_simd == equal_vec.begin()); // 2 should be inserted at index 0
+    EXPECT_EQ(it_equal_default_simd, equal_vec.begin());
 
     it_equal_default_simd = jrmwng::algorithm::simd::lower_bound(equal_vec, 1);
-    assert(it_equal_default_simd == equal_vec.begin()); // 1 should be inserted at index 0
+    EXPECT_EQ(it_equal_default_simd, equal_vec.begin());
 
     it_equal_default_simd = jrmwng::algorithm::simd::lower_bound(equal_vec, 3);
-    assert(it_equal_default_simd == equal_vec.end()); // 3 should be inserted at index 4
+    EXPECT_EQ(it_equal_default_simd, equal_vec.end());
+}
 
-    // Test SIMD lower_bound with floats
+TEST(LowerBoundSimdTest, Floats) {
     std::vector<float> vec_f = {1.1f, 2.2f, 4.4f, 5.5f, 6.6f};
     auto it_f_simd = jrmwng::algorithm::simd::lower_bound(vec_f, 3.3f);
-    assert(it_f_simd == vec_f.begin() + 2); // 3.3 should be inserted at index 2
+    EXPECT_EQ(it_f_simd, vec_f.begin() + 2);
+}
 
-    // Test SIMD lower_bound with custom predicate for floats
+TEST(LowerBoundSimdTest, CustomPredicateFloats) {
     struct CustomFloatType {
         float value;
         bool operator<(const CustomFloatType& other) const {
@@ -95,74 +115,85 @@ void test_lower_bound_simd() {
     std::vector<CustomFloatType> custom_vec_f = {{1.1f}, {3.3f}, {5.5f}};
     CustomFloatType new_value_f = {4.4f};
     auto it_custom_f_simd = jrmwng::algorithm::simd::lower_bound(custom_vec_f, new_value_f, std::less<CustomFloatType>());
-    assert(it_custom_f_simd == custom_vec_f.begin() + 2); // {4.4} should be inserted at index 2
+    EXPECT_EQ(it_custom_f_simd, custom_vec_f.begin() + 2);
+}
 
-    // Test SIMD lower_bound with empty vector of floats
+TEST(LowerBoundSimdTest, EmptyVectorFloats) {
     std::vector<float> empty_vec_f;
     auto it_empty_f_simd = jrmwng::algorithm::simd::lower_bound(empty_vec_f, 1.1f);
-    assert(it_empty_f_simd == empty_vec_f.begin()); // 1.1 should be inserted at index 0
+    EXPECT_EQ(it_empty_f_simd, empty_vec_f.begin());
+}
 
-    // Test SIMD lower_bound with single element vector of floats
+TEST(LowerBoundSimdTest, SingleElementVectorFloats) {
     std::vector<float> single_vec_f = {2.2f};
     auto it_single_f_simd = jrmwng::algorithm::simd::lower_bound(single_vec_f, 1.1f);
-    assert(it_single_f_simd == single_vec_f.begin()); // 1.1 should be inserted at index 0
+    EXPECT_EQ(it_single_f_simd, single_vec_f.begin());
 
     it_single_f_simd = jrmwng::algorithm::simd::lower_bound(single_vec_f, 3.3f);
-    assert(it_single_f_simd == single_vec_f.end()); // 3.3 should be inserted at index 1
+    EXPECT_EQ(it_single_f_simd, single_vec_f.end());
+}
 
-    // Test SIMD lower_bound with all elements equal for floats
+TEST(LowerBoundSimdTest, AllElementsEqualFloats) {
     std::vector<float> equal_vec_f = {2.2f, 2.2f, 2.2f, 2.2f};
     auto it_equal_f_simd = jrmwng::algorithm::simd::lower_bound(equal_vec_f, 2.2f);
-    assert(it_equal_f_simd == equal_vec_f.begin()); // 2.2 should be inserted at index 0
+    EXPECT_EQ(it_equal_f_simd, equal_vec_f.begin());
 
     it_equal_f_simd = jrmwng::algorithm::simd::lower_bound(equal_vec_f, 1.1f);
-    assert(it_equal_f_simd == equal_vec_f.begin()); // 1.1 should be inserted at index 0
+    EXPECT_EQ(it_equal_f_simd, equal_vec_f.begin());
 
     it_equal_f_simd = jrmwng::algorithm::simd::lower_bound(equal_vec_f, 3.3f);
-    assert(it_equal_f_simd == equal_vec_f.end()); // 3.3 should be inserted at index 4
+    EXPECT_EQ(it_equal_f_simd, equal_vec_f.end());
+}
 
-    // Test SIMD lower_bound with custom projection for integers
+TEST(LowerBoundSimdTest, CustomProjectionIntegers) {
     struct CustomIntType {
         int value;
     };
 
     std::vector<CustomIntType> custom_vec_int = {{1}, {3}, {5}};
     auto it_custom_proj_int = jrmwng::algorithm::simd::lower_bound(custom_vec_int, 4, std::less<int>(), [](const CustomIntType& ct) { return ct.value; });
-    assert(it_custom_proj_int == custom_vec_int.begin() + 2); // {4} should be inserted at index 2
+    EXPECT_EQ(it_custom_proj_int, custom_vec_int.begin() + 2);
+}
+
+TEST(LowerBoundSimdTest, CustomProjectionFloats) {
+    struct CustomFloatType {
+        float value;
+    };
 
     std::vector<CustomFloatType> custom_vec_float = {{1.1f}, {3.3f}, {5.5f}};
     auto it_custom_proj_float = jrmwng::algorithm::simd::lower_bound(custom_vec_float, 4.4f, std::less<float>(), [](const CustomFloatType& ct) { return ct.value; });
-    assert(it_custom_proj_float == custom_vec_float.begin() + 2); // {4.4} should be inserted at index 2
+    EXPECT_EQ(it_custom_proj_float, custom_vec_float.begin() + 2);
+}
 
-    // Test SIMD lower_bound with custom projection for doubles
+TEST(LowerBoundSimdTest, CustomProjectionDoubles) {
     struct CustomDoubleType {
         double value;
     };
 
     std::vector<CustomDoubleType> custom_vec_double = {{1.1}, {3.3}, {5.5}};
     auto it_custom_proj_double = jrmwng::algorithm::simd::lower_bound(custom_vec_double, 4.4, std::less<double>(), [](const CustomDoubleType& ct) { return ct.value; });
-    assert(it_custom_proj_double == custom_vec_double.begin() + 2); // {4.4} should be inserted at index 2
-
-    // Test SIMD lower_bound with SIMD projection for integers
-    std::vector<int> vec_int = {1, 2, 4, 5, 6};
-    auto it_simd_proj_int = jrmwng::algorithm::simd::lower_bound(vec_int, 3, std::less<int>(), [](const __m256i& v) { return v; });
-    assert(it_simd_proj_int == vec_int.begin() + 2); // 3 should be inserted at index 2
-
-    // Test SIMD lower_bound with SIMD projection for floats
-    std::vector<float> vec_float = {1.1f, 2.2f, 4.4f, 5.5f, 6.6f};
-    auto it_simd_proj_float = jrmwng::algorithm::simd::lower_bound(vec_float, 3.3f, std::less<float>(), [](const __m256& v) { return v; });
-    assert(it_simd_proj_float == vec_float.begin() + 2); // 3.3 should be inserted at index 2
-
-    // Test SIMD lower_bound with SIMD projection for doubles
-    std::vector<double> vec_double = {1.1, 2.2, 4.4, 5.5, 6.6};
-    auto it_simd_proj_double = jrmwng::algorithm::simd::lower_bound(vec_double, 3.3, std::less<double>(), [](const __m256d& v) { return v; });
-    assert(it_simd_proj_double == vec_double.begin() + 2); // 3.3 should be inserted at index 2
-
-    std::cout << "All SIMD tests passed!" << std::endl;
+    EXPECT_EQ(it_custom_proj_double, custom_vec_double.begin() + 2);
 }
 
-int main() {
-    std::cout << "Compilation date and time: " << __DATE__ << " " << __TIME__ << std::endl;
-    test_lower_bound_simd();
-    return 0;
+TEST(LowerBoundSimdTest, SimdProjectionIntegers) {
+    std::vector<int> vec_int = {1, 2, 4, 5, 6};
+    auto it_simd_proj_int = jrmwng::algorithm::simd::lower_bound(vec_int, 3, std::less<int>(), [](const __m256i& v) { return v; });
+    EXPECT_EQ(it_simd_proj_int, vec_int.begin() + 2);
+}
+
+TEST(LowerBoundSimdTest, SimdProjectionFloats) {
+    std::vector<float> vec_float = {1.1f, 2.2f, 4.4f, 5.5f, 6.6f};
+    auto it_simd_proj_float = jrmwng::algorithm::simd::lower_bound(vec_float, 3.3f, std::less<float>(), [](const __m256& v) { return v; });
+    EXPECT_EQ(it_simd_proj_float, vec_float.begin() + 2);
+}
+
+TEST(LowerBoundSimdTest, SimdProjectionDoubles) {
+    std::vector<double> vec_double = {1.1, 2.2, 4.4, 5.5, 6.6};
+    auto it_simd_proj_double = jrmwng::algorithm::simd::lower_bound(vec_double, 3.3, std::less<double>(), [](const __m256d& v) { return v; });
+    EXPECT_EQ(it_simd_proj_double, vec_double.begin() + 2);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
